d61d597ec591d1fa3f1dec1d26e743b5ad7db091
Carson McKinstry <carson.mckinstry@gmail.com>
2019-10-03 15:30:59 +0200

    unified the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const merge = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const buildPath = path.join(__dirname, 'build');
 
@@ -20,7 +24,51 @@ const baseConfig = {
   plugins: [new Dotenv()]
 };
 
-const appConfig = merge(baseConfig, {});
+const appConfig = merge(baseConfig, {
+  entry: path.join(__dirname, 'app/index.ts'),
+  output: {
+    filename: 'app.js',
+    path: buildPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      { test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/, loader: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'app/views/index.ejs'),
+      meta: {
+        viewport: 'width=device-width, initial-scale=1'
+      }
+    }),
+    new CopyPlugin([
+      {
+        from: path.join(__dirname, 'app/assets'),
+        to: path.resolve(__dirname, 'build/assets')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: './styles.css'
+    })
+  ],
+  target: 'electron-renderer'
+});
 
 const mainConfig = merge(baseConfig, {
   entry: path.join(__dirname, './main.ts'),
@@ -35,4 +83,4 @@ const mainConfig = merge(baseConfig, {
   target: 'electron-main'
 });
 
-module.exports = [mainConfig];
+module.exports = [mainConfig, appConfig];
